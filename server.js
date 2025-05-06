16f5f6f01fdaf741f8c70e2981b164e836b991c1
skillneurs <skillneurs@gmail.com>
2025-05-06 14:13:19 +0200

    ok

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connexion à MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('✅ Connexion MongoDB réussie'))
   .catch((err) => console.error('❌ Erreur MongoDB :', err));
 
 
-// Middlewares
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Accès public aux fichiers
